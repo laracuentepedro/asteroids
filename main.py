60921f5a3ab2c76a980a753187c06dcd29f31e86
Pedro Laracuente <laracuentepedro@gmail.com>
2025-09-22 19:14:41 -0400

    Add constants for game configuration and update main function message

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 def main():
-    print("Hello from asteroids!")
+    print("Starting Asteroids!")
 
 
 if __name__ == "__main__":
